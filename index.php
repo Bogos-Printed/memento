1b2d7217fe6ea1f394daf9f23a73d12ff36a3902
Bogos-Printed <beauclair.nat@gmail.com>
2023-11-20 14:10:22 +0100

    Added font & fixed button

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Pixelify+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Handlee&family=Nunito&display=swap" rel="stylesheet">
     <title>Memento stuff</title>
     <style>
         *{
@@ -32,22 +34,28 @@
         </section>
         <section id="post-area">
             <div class="note container">
-                <h2>title</h2>
-                <input type="button" value="X" alt="delete button">
+                <div class="title_button">
+                    <h2>title</h2>
+                    <input type="button" value="X" alt="delete button">
+                </div>
                 <p>paragraph</p>
                 <p>paragraph</p>
                 <p>paragraph</p>
                 <p>paragraph</p>
             </div>
             <div class="note container">
-                <h2>title</h2>
-                <input type="button" value="X" alt="delete button">
+                <div class="title_button">
+                    <h2>title</h2>
+                    <input type="button" value="X" alt="delete button">
+                </div>
                 <p>paragraph</p>
                 <p>paragraph</p>
             </div>
             <div class="note container">
-                <h2>title</h2>
-                <input type="button" value="X" alt="delete button">
+                <div class="title_button">
+                    <h2>title</h2>
+                    <input type="button" value="X" alt="delete button">
+                </div>
                 <p>paragraph</p>
                 <a href="#">some link to a website</a>
             </div>
