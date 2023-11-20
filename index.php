990b2c277d063febaa42d98c934d38f9a1033ea3
Bogos-Printed <beauclair.nat@gmail.com>
2023-11-20 10:14:05 +0100

    structure of project

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h2>Memento</h2>
+        <nav>
+            <a href="">Login</a>
+            <a href="">Register</a>
+        </nav>
+    </header>
+    <main>
+        <article>
+            <h2>title</h2>
+            <p>paragraph</p>
+            <p>paragraph</p>
+            <p>paragraph</p>
+            <p>paragraph</p>
+        </article>
+    </main>
+</body>
+</html>
