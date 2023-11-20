ebe105292c85ac89e08ce9458aeb17ca1fc9b974
Bogos-Printed <beauclair.nat@gmail.com>
2023-11-20 12:28:51 +0100

    Made the front side, need fix on button!

--- index.php
+++ index.php
@@ -8,24 +8,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Memento stuff</title>
+    <style>
+        *{
+            outline: 2px solid black;
+        }
+    </style>
 </head>
+
 <body>
     <header>
-        <h2>Memento</h2>
+        <p>Memento</p>
         <nav>
             <a href="">Login</a>
             <a href="">Register</a>
         </nav>
     </header>
     <main>
-        <article>
-            <h2>title</h2>
-            <p>paragraph</p>
-            <p>paragraph</p>
-            <p>paragraph</p>
-            <p>paragraph</p>
-        </article>
+        <section class="top">
+            <h1>Memento</h1>
+            <input type="button" onclick="" value="Add Post-It">
+        </section>
+        <section id="post-area">
+            <div class="note container">
+                <h2>title</h2>
+                <input type="button" value="X" alt="delete button">
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+            </div>
+            <div class="note container">
+                <h2>title</h2>
+                <input type="button" value="X" alt="delete button">
+                <p>paragraph</p>
+                <p>paragraph</p>
+            </div>
+            <div class="note container">
+                <h2>title</h2>
+                <input type="button" value="X" alt="delete button">
+                <p>paragraph</p>
+                <a href="#">some link to a website</a>
+            </div>
+        </section>
+
     </main>
 </body>
 </html>
